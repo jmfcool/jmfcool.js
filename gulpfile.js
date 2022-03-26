ede625fa21aa5ffbec27cc1789b21c5848bb042e
Jeremiah Faria <jmfcool@me.com>
2022-03-26 08:13:19 -0700

    Update repo from gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,7 @@ const minify = require('gulp-minify');
 const jest = require('gulp-jest').default;
 const git = require('gulp-git');
 
-gulp.task('release', function() 
-{
+gulp.task('release', () => {
     return gulp.src('jmfcool.js')
     .pipe(minify({
         ext:{
@@ -15,8 +14,7 @@ gulp.task('release', function()
     .pipe(gulp.dest('release'))
 });
 
-gulp.task('demo', function() 
-{
+gulp.task('demo', () => {
     return gulp.src('jmfcool.js')
     .pipe(minify({
         ext:{
@@ -29,8 +27,7 @@ gulp.task('demo', function()
 
 gulp.task('build', gulp.series('release','demo'));
 
-gulp.task('jest', function() 
-{
+gulp.task('jest', () => {
     return gulp.src('test').pipe(jest({
       "testEnvironment": "jsdom",
       "verbose": true,
@@ -42,31 +39,25 @@ gulp.task('jest', function()
     }));
 });
 
-gulp.task('pull', function(done) 
-{
-    git.pull('origin', 'main', function (err) 
-    {
+gulp.task('pull', (done) => {
+    git.pull('origin', 'main', (err) => {
         if (err) throw err;
     });
     done();
 });
 
-gulp.task('add', function() 
-{
+gulp.task('add', () => {
     return gulp.src('.')
     .pipe(git.add());
 });
 
-gulp.task('commit', function() 
-{
+gulp.task('commit', () => {
     return gulp.src('.')
       .pipe(git.commit('Update repo from gulp'));
 });
 
-gulp.task('push', function(done) 
-{
-    git.push('origin', function (err) 
-    {
+gulp.task('push', (done) => {
+    git.push('origin', (err) => {
         if (err) throw err;
     });
     done();
@@ -74,10 +65,8 @@ gulp.task('push', function(done)
 
 gulp.task('git', gulp.series('add','commit','push'));
 
-gulp.task('tags', function(done)
-{
-    git.push('origin', { args: ' --tags' }, function (err) 
-    {
+gulp.task('tags', (done) => {
+    git.push('origin', { args: ' --tags' }, (err) => {
         if (err) throw err;
     });
     done();
