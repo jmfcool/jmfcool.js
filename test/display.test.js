17f275e0073e4e0d918bc18d26b3f43e90d32732
Jeremiah Faria <jmfcool@me.com>
2022-03-27 11:32:04 -0700

    Update repo from gulp

--- test/display.test.js
+++ test/display.test.js
@@ -1,6 +1,37 @@
 import { test } from '@jest/globals';
 import { jmfcool } from '../jmfcool.js';
 
+const model = jest.fn(model => model);
+
+jmfcool.model = (file, callback) => {
+
+    const data = {
+        "user" : 
+        {
+            "firstName" : "John",
+            "lastName" : "Doe"
+        },
+        "item" : 
+        {
+            "cost" : 18.5000,
+            "name" : "Oranges"
+        }
+    };
+
+    callback(data);
+
+};
+
+const view = jest.fn(view => view);
+
+jmfcool.view = (file, callback) => {
+
+    const data = '<div class="item"><p>Your price for ${item.name} is $${item.cost?currency}!</p></div>';
+
+    callback(data);
+
+};
+
 describe('Display', () => {
 
     test('should be a function', () => {
@@ -8,11 +39,17 @@ describe('Display', () => {
     }); 
 
     test('displays rendered view', () => {
+        jmfcool.model('model.json', model);
+        jmfcool.view('item.view', view);
+
         const spy = jest.spyOn(jmfcool, 'display');
-        audio.volume = 100;
+        
+        const display = jmfcool.display({ view:view.mock.results[0].value, model:model.mock.results[0].value, hook:'response-user' });
       
-        expect(spy).toHaveBeenCalled();
-        expect(audio.volume).toBe(100);
+        console.log('display: ', display);
+
+        //expect(spy).toHaveBeenCalled();
+        //expect(audio.volume).toBe(100);
       
         spy.mockRestore();
       });
