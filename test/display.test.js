6a9db7b51218c4629f56006c74ca1c41f67eb7c8
Jeremiah Faria <jmfcool@me.com>
2022-03-27 11:27:28 -0700

    Update repo from gulp

--- test/display.test.js
+++ test/display.test.js
@@ -6,5 +6,15 @@ describe('Display', () => {
     test('should be a function', () => {
         expect(jmfcool.display).toBeInstanceOf(Function);
     }); 
+
+    test('displays rendered view', () => {
+        const spy = jest.spyOn(jmfcool, 'dis'); // we pass 'set'
+        audio.volume = 100;
+      
+        expect(spy).toHaveBeenCalled();
+        expect(audio.volume).toBe(100);
+      
+        spy.mockRestore();
+      });
         
 });
